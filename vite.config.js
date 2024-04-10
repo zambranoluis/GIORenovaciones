b6771ca4b0c1f2a5c9358ad2d99e5cc451634503
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2024-04-10 17:24:17 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/nombre-del-repo/',
   plugins: [react()],
+	base: '/GIORenovaciones/',
 	// server: {
 	// 	port: 3000,
 	// }
